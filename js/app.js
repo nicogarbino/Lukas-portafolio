d7aa2278bfc0cbce7c92018184790ec87b289df1
Nicolas Garbino <nicolasgarbino@gmail.com>
2021-08-19 12:39:43 -0300

    Menu responsive

--- js/app.js
+++ js/app.js
@@ -13,3 +13,9 @@ window.addEventListener('scroll', function(){
     }
 });
 
+function toggleMenu () {
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
